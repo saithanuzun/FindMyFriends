3099108f97eb50b952b21b2f1f265a79bd82f2f0
saithanuzun <61024473+saithanuzun@users.noreply.github.com>
2022-06-22 23:11:14 +0100

    .

---
+++ FindMyFriends/FindMyFriends/Services/Connection.cs
@@ -0,0 +1,14 @@
+﻿using FindMyFriends.Services;
+using Firebase.Database;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FoodOrderingApp.Services
+{
+    public class Connection
+    {
+        FirebaseClient firebaseClient = new Firebase.Database.FirebaseClient(Constants.FirebaseApiUrl);
+
+    }
+}
---
+++ FindMyFriends/FindMyFriends/Services/Constants.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FindMyFriends.Services
+{
+    public class Constants
+    {
+        public const string FirebaseApiUrl = "https://findmyfriends-7a08b-default-rtdb.europe-west1.firebasedatabase.app/";
+    }
+}
